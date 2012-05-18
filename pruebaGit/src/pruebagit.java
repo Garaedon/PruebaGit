ab3b8ae6b61950cb16730fff9b9942d8406e139a
cadorado <cadorado@106-02.info2>
2012-05-18 13:39:46 +0200

    tercera

--- pruebaGit/src/pruebagit.java
+++ pruebaGit/src/pruebagit.java
@@ -4,5 +4,7 @@ public class pruebagit
  public static void main(String[]args)
  {
 	System.out.println("primera linea");
+	System.out.println("segunda linea");
+	System.out.println("tercera linea");
  }
 }
